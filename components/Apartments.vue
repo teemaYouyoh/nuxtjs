77a03f5919569ca6879e0c8d1e999997645087a1
marsiuser <marsiparit@gmail.com>
2020-12-23 20:05:16 +0200

    arina 23-12-20

---
+++ components/Apartments.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="apartments">
+       <div class="apartments-slider">
+           <p>01</p>
+           <p>02</p>
+           <p class="partments-slider__current">03</p>
+           <p>04</p>
+       </div>
+       <div class="apartments_container _container">
+           <p>Ипотеке по ставке 0,01%</p>
+           <h1>Квартиры 4,5 млн за 42 м2</h1>
+           <p>10 минут  от метро</p>
+           <ul>
+               <li>Домодедовская</li>
+               <li>Красногвардейская</li>
+           </ul>
+       </div>
+   </div>
+</template>
+
+<style>
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <div class="header-container _container">
+           <div class="header-logo">
+               <a href="#">
+                    <img src="../assets/img/logo.svg" alt="logo">
+                    <span>Три Квартала</span>
+               </a>
+           </div>
+           <div class="header-menu">
+               <nav>
+                   <ul>
+                       <li><a href="#">о квартале</a></li>
+                       <li><a href="#">Кварталы</a></li>
+                       <li><a href="#">локация</a></li>
+                       <li><a href="#">Преимущества</a></li>
+                       <li><a href="#">инфраструктура</a></li>
+                       <li><a href="#">контакты</a></li>
+                   </ul>
+               </nav>
+           </div>
+           <div class="header-phone">
+               <a href="tel:+88005005005">8 800 500 500 5</a>
+           </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
